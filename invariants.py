73773d55a2342f81fdf0cb59f74256a2b1c45e73
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-05-11 16:47:09 +0100

    Verification program for 2 maximum elements

--- invariants.py
+++ invariants.py
@@ -1,4 +1,20 @@
-def assertInvariant(s, i, invariant, n=None): #n is value or collection
-    if n == None: assert(s == invariant(i))
-    else: assert(s == invariant(i, n))
+from multipledispatch import dispatch
+from typing import List
+
+@dispatch(int, int, object, int)
+def assertInvariant(s, i, invariant, n):
+    assert(s == invariant(i, n))
+
+@dispatch(int, int, object, object)
+def assertInvariant(s, i, invariant, arr):
+    assert(s == invariant(i, arr))
+
+@dispatch(int, int, object, object, int)
+def assertInvariant(s, i, invariant, arr, n):
+    assert(s == invariant(i, arr, n))
+
+@dispatch(int, int, object)
+def assertInvariant(s, i, invariant):
+    assert(s == invariant(i))
+
 
---
+++ maxelement.py
@@ -0,0 +1,64 @@
+import invariants
+from multipledispatch import dispatch
+
+def maxElem(arr):
+    n = len(arr)
+
+    if n == 1:
+        return arr[0]
+
+    m = arr[0]
+
+    for i in range(1, n):
+        invariants.assertInvariant(m, i, maxElemInvariant, arr)
+        if m < arr[i]:
+            m = arr[i]
+
+    return m
+
+def getYComplementUnordered(arr, arrSet):
+    a = []
+    for item in arr:
+        if item in arrSet:
+            a.append(item)
+
+    return a
+
+def max2Elems(arr):
+    n = len(arr)
+
+    assert(n >= 2)
+
+    if arr[0] <= arr[1]:
+        x = arr[0]
+        y = arr[1]
+    else:
+        x = arr[1]
+        y = arr[0]
+
+    for i in range(2, n):
+        invariants.assertInvariant(y, i, maxElemInvariant, arr)
+        
+        invariants.assertInvariant(x, i, maxElemInvariant, arr, y)
+        if y < arr[i]:
+            x = y
+            y = arr[i]
+        else:
+            if x < arr[i]:
+                x = arr[i]
+
+
+    return (x,y)
+
+@dispatch(int, object)
+def maxElemInvariant(i, arr):
+    print(arr)
+    return max(arr[0:i])
+
+@dispatch(int, object, int)
+def maxElemInvariant(i, arr, n):
+    y_compl = list(set(arr[0:i])-set([n]))
+    a = getYComplementUnordered(arr[0:i], y_compl)
+    return max(a)
+
+print(max2Elems([1,5,42,-5,6,7,106,2,4,5,8,9]))
